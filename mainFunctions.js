d94ce18b6e5802a2bb9920de249e8fc777f5322e
Jiri Nemecek <contact@nemecekjiri.cz>
2020-02-23 19:58:26 +0100

    Chrome mobile hacks and tricks done
    TODO: Check other Mobile version issues

--- mainFunctions.js
+++ mainFunctions.js
@@ -266,3 +266,17 @@ $('#map').click(e => {
 	$("#add_point_name_input").blur();
 	$("#add_point_img_picker").blur();
 });
+
+// Successful workaround for certain mobile browsers to use proper vh for body
+// without the top bar.
+// We listen to the resize event
+window.addEventListener('resize', () => {
+	// We execute the same script as before
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
